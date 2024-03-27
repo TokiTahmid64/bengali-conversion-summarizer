52409d93c20884b5e2f7b833eefbc23915acdd8e
salkhon <salmankhondker1998@gmail.com>
2024-03-28 01:57:56 +0600

    updated wav path

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.transcribe.diarize import diarize_audio
 from src.transcribe.transcribe import transcribe_audiofiles
 
 if __name__ == "__main__":
-    diarized_files = diarize_audio("/home/salkhon/repo/bn-conv-summarizer/talkshow.wav")
+    diarized_files = diarize_audio("./talkshow.wav")
     conversation = transcribe_audiofiles(diarized_files)
     print(conversation)
