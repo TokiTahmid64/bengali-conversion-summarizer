43330c37866f41f8de936b838034860e1f2b1e4f
TokiTahmid64 <sharifulislamtoki@gmail.com>
2024-06-25 23:22:30 +0600

    updated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.transcribe.diarize import diarize_audio
 from src.transcribe.transcribe import transcribe_audiofiles
-from src.summarize.summarize import summarize_conversation
 import sys
 import shutil
 
@@ -10,11 +9,19 @@ if __name__ == "__main__":
     diarized_files = diarize_audio(audio_path)
     conversation = transcribe_audiofiles(diarized_files, model_path)
     conv_str = "\n".join([f"{name}: {text}" for name, text in conversation])
-    summary = summarize_conversation(conv_str)
 
-    # delete the diarized files
-    shutil.rmtree("dia_output")
 
-    # save the summary
-    with open("summary.txt", "w") as f:
-        f.write(summary)
+    # save the conversation in a file. Please rename the file to the name of the audio file
+
+    with open("{audio_path}.txt", "w") as f:
+        f.write(conv_str)
+
+
+
+
+    # # delete the diarized files
+    # shutil.rmtree("dia_output")
+
+    # # save the summary
+    # with open("summary.txt", "w") as f:
+    #     f.write(summary)
